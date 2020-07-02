7a732f255f3232dccf90b1a8442b48725d0f44a8
EtsuN <etnakaha@ucsd.edu>
2020-07-03 06:37:53 +0800

    keypad code

---
+++ Assets/Keypad/Script/Playsound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playsound : MonoBehaviour 
+{
+    public char number;
+
+	public void Clicky (){
+		GetComponent<AudioSource>().Play();
+        this.transform.parent.parent.Find("KeypadCanvas").GetComponent<KeypadCanvas>().enterPasscode(number);
+	}
+
+
+}
---
+++ Assets/Scripts/KeypadCanvas.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeypadCanvas : MonoBehaviour {
+
+    public string ANSWER;
+    private string input;
+
+    //game master
+    public GameMaster gm;
+
+    // Use this before initialization
+    void Awake() {
+        input = "";
+        gm = GameObject.Find("Player").GetComponent<GameMaster>();
+    }
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+    public void enterPasscode(char number)
+    {
+        if (number == 'c')
+        {
+            input = "";
+        }
+        else if (number == 'x')
+        {
+            closeKeypad();
+        }
+        else
+        {
+            if (input.Length == 3)
+                input = "";
+            input += number;
+
+            if (input == ANSWER)
+            {
+                Debug.LogError("Correct answer");
+                closeKeypad();
+                gm.changeRoom();
+            }
+            else if (input.Length == 3)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+
+    private void closeKeypad()
+    {
+        input = "";
+        gm.disableKeypadCanvas(this.transform.gameObject);
+    }
+}
